a65789969d89be5caed88f424d339227185a2bed
VedanshiAwasthi <vedanshi.awasthi@rippling.com>
2025-05-25 19:06:09 +0530

    Initial commit - Land Registry System

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+
+
+
+import Homee from "../Components/home1"
+import FrontPage from "@/Components/FrontPage"; 
+import  Footer  from "@/Components/Footer";
+
+const Home = () => {
+    return (
+      <>
+        <FrontPage/>
+        <Homee />
+        <Footer/>
+      </>
+    );
+  };
+
+  export default Home;
